798c6b3794e06258ec01fe1de489bf697837784b
Sneha-Shaw-Itobuz <sneha@itobuz.com>
2023-03-02 14:56:02 +0530

    `third commit`

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
-const locationName = document.querySelector("h3");
+const locationName = document.querySelector("input");
 const degree = document.querySelector(".degree h4");
 const feels = document.querySelector(".degree p");
 let response;
+let path;
+const icon = document.querySelector(".icon img");
 
-const otherInfo = document.querySelector(".info");
-
-async function getInfo() {
+// get weather data using api
+async function getWeatherData(query) {
   response = await fetch(
-    "http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=Kolkata&aqi=no"
+    `http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=${query}&aqi=no`
   )
     .then((data) => {
       return data.json();
@@ -15,20 +16,28 @@ async function getInfo() {
     .catch((err) => {
       console.log(err);
     });
-  console.log(response);
-  addData();
+  addWeatherData();
 }
 
-getInfo();
-
-function addData() {
-  locationName.innerHTML = `${response.location.name}`;
+function addWeatherData() {
+  locationName.value = `${response.location.name}`;
   degree.innerHTML = `${response.current.temp_c}<sup>o</sup>`;
   feels.innerHTML = `Feels ${response.current.feelslike_c}<sup>o</sup>`;
-
-  otherInfo.innerHTML = `
-    <div>Country: ${response.location.country}</div>
-    <div>Humidity: ${response.current.humidity}</div>
-    <div>Wind Speed: ${response.current.wind_mph} MPH</div>
-    `;
+  path = response.current.condition.icon.replace(
+    "//cdn.weatherapi.com",
+    "../assets"
+  );
+  path = path.replace(".png", ".svg");
+  // icon.src = response.current.condition.icon;
+  icon.src = path;
 }
+
+locationName.addEventListener("keyup", (e) => {
+  if (e.key === "Enter") {
+    getWeatherData(e.target.value);
+  }
+});
+
+window.onload = () => {
+  getWeatherData("Kolkata");
+};
