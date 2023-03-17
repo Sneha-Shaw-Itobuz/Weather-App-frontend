91040e2bde94f5a5e4bb2a2a7f1f1544ae11d5e5
Sneha-Shaw-Itobuz <sneha@itobuz.com>
2023-03-17 10:20:00 +0530

    connected to backend

--- js/script.js
+++ js/script.js
@@ -6,16 +6,16 @@ const suggestion = document.querySelector(".suggestion");
 
 let iconPath;
 
-const allCities = ["Kolkata", "London", "Mumbai", "Chennai", "Pune"];
+const allCities = [];
 
 // get weather data using api
 async function getWeatherData(query) {
-  console.log(query.length);
   if (query.length !== 0) {
+    console.log(query);
     // let response = await fetch(
     //   `http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=${query}&aqi=no`
     // )
-    let response = await fetch(`http://localhost:5000/?city=${query}`)
+    await fetch(`http://localhost:5000/get-weather?city=${query}`)
       .then((data) => {
         return data.json();
       })
@@ -31,17 +31,19 @@ async function getWeatherData(query) {
 
 function addWeatherData(res) {
   console.log(res);
-  locationName.value = `${res.location.name}`;
-  degree.innerHTML = `${res.current.tempC}<sup>o</sup>`;
-  feels.innerHTML = `Feels ${res.current.feelslikeC}<sup>o</sup>`;
+  locationName.value = `${res.data.location.name}`;
+  degree.innerHTML = `${res.data.current.tempC}<sup>o</sup>`;
+  feels.innerHTML = `Feels ${res.data.current.feelslikeC}<sup>o</sup>`;
 
-  // iconPath = res.current.condition.icon.replace(
-  //   "//cdn.weatherapi.com",
-  //   "../assets"
-  // );
-  // iconPath = iconPath.replace(".png", ".svg");
-
-  // icon.src = iconPath;
+  if (res.data.current.cloud < 20) {
+    icon.src = "../assets/weather/64x64/day/113.svg";
+  } else if (res.data.current.cloud > 20 && res.data.current.cloud < 50) {
+    icon.src = "../assets/weather/64x64/day/116.svg";
+  } else if (res.data.current.cloud > 50 && res.data.current.cloud < 80) {
+    icon.src = "../assets/weather/64x64/day/119.svg";
+  } else if (res.data.current.cloud > 80 && res.data.current.cloud < 101) {
+    icon.src = "../assets/weather/64x64/day/143.svg";
+  }
 }
 
 locationName.addEventListener("keyup", (e) => {
@@ -51,16 +53,6 @@ locationName.addEventListener("keyup", (e) => {
   }
 });
 
-(function addSuggestion() {
-  allCities.forEach((city) => {
-    let li = document.createElement("li");
-    li.textContent = city;
-    let hr = document.createElement("hr");
-    suggestion.firstElementChild.appendChild(li);
-    suggestion.firstElementChild.appendChild(hr);
-  });
-})();
-
 suggestion.firstElementChild.addEventListener("click", (e) => {
   locationName.value = e.target.textContent;
 
@@ -68,13 +60,42 @@ suggestion.firstElementChild.addEventListener("click", (e) => {
 });
 
 window.addEventListener("click", (e) => {
-  if (e.target === locationName) {
+  if (e.target === locationName || e.target == suggestion) {
     suggestion.classList.remove("hide");
   } else {
     suggestion.classList.add("hide");
   }
 });
 
+async function getAllCities() {
+  await fetch("http://localhost:5000/all-cities")
+    .then((data) => {
+      return data.json();
+    })
+    .then((d) => {
+      d.forEach((city) => {
+        allCities.push(city);
+      });
+    })
+    .then(() => addSuggestion())
+    .catch((err) => {
+      console.log(err);
+      alert(err);
+    });
+}
+
+function addSuggestion() {
+  allCities.forEach((city) => {
+    let li = document.createElement("li");
+    li.textContent = city;
+    let hr = document.createElement("hr");
+    suggestion.firstElementChild.appendChild(li);
+    suggestion.firstElementChild.appendChild(hr);
+  });
+}
+
 (() => {
+  // addSuggestion()
+  getAllCities();
   getWeatherData("Kolkata");
 })();
